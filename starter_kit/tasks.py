fd56304ff08f7b781dde71f120ce22e8f3b54bde
tquang@mysuki.io <tquang@mysuki.io>
2024-12-20 23:13:14 +0800

    added new task

--- starter_kit/tasks.py
+++ starter_kit/tasks.py
@@ -19,7 +19,7 @@ def transfer(w3: Web3) -> None:
     """Transfers 0.01 NTN to a recipient specified in .env."""
 
     autonity = Autonity(w3)
-    amount = int(0.01 * 10 ** autonity.decimals())
+    amount = int(10.01 * 10 ** autonity.decimals())
     tx = autonity.transfer(params.RECIPIENT_ADDRESS, amount).transact()
     w3.eth.wait_for_transaction_receipt(tx)
 
@@ -32,7 +32,7 @@ def bond(w3: Web3) -> None:
 
     autonity = Autonity(w3)
     validator_address = autonity.get_validators()[0]
-    amount = int(0.01 * 10 ** autonity.decimals())
+    amount = int(10.01 * 10 ** autonity.decimals())
     tx = autonity.bond(validator_address, amount).transact()
     w3.eth.wait_for_transaction_receipt(tx)
 
@@ -45,7 +45,7 @@ def unbond(w3: Web3) -> None:
 
     autonity = Autonity(w3)
     validator_address = autonity.get_validators()[0]
-    amount = int(0.01 * 10 ** autonity.decimals())
+    amount = int(10.01 * 10 ** autonity.decimals())
     tx = autonity.unbond(validator_address, amount).transact()
     w3.eth.wait_for_transaction_receipt(tx)
 
@@ -57,7 +57,7 @@ def approve(w3: Web3) -> None:
     """Approves the transfer of 0.01 NTN by a recipient specified in .env."""
 
     autonity = Autonity(w3)
-    amount = int(0.01 * 10 ** autonity.decimals())
+    amount = int(10.01 * 10 ** autonity.decimals())
     tx = autonity.approve(params.RECIPIENT_ADDRESS, amount).transact()
     w3.eth.wait_for_transaction_receipt(tx)
 
@@ -69,7 +69,7 @@ def swap_exact_tokens_for_tokens(w3: Web3) -> None:
     """Swaps 0.01 NTN for USDCx."""
 
     ntn = ERC20(w3, params.NTN_ADDRESS)
-    ntn_amount = int(0.01 * 10 ** ntn.decimals())
+    ntn_amount = int(10.01 * 10 ** ntn.decimals())
     approve_tx = ntn.approve(params.UNISWAP_ROUTER_ADDRESS, ntn_amount).transact()
     w3.eth.wait_for_transaction_receipt(approve_tx)
 
@@ -93,7 +93,7 @@ def swap_exact_atn_for_ntn(w3: Web3) -> None:
     """Swaps 0.01 ATN for NTN."""
 
     watn = ERC20(w3, params.WATN_ADDRESS)
-    atn_amount = int(0.01 * 10 ** watn.decimals())
+    atn_amount = int(10.01 * 10 ** watn.decimals())
     approve_tx = watn.approve(params.UNISWAP_ROUTER_ADDRESS, atn_amount).transact()
     w3.eth.wait_for_transaction_receipt(approve_tx)
 
@@ -116,12 +116,12 @@ def add_liquidity(w3: Web3) -> None:
     """Adds 0.1 NTN and 0.01 USDCx to the Uniswap liquidity pool."""
 
     ntn = ERC20(w3, params.NTN_ADDRESS)
-    ntn_amount = int(0.1 * 10 ** ntn.decimals())
+    ntn_amount = int(10.1 * 10 ** ntn.decimals())
     approve_tx_2 = ntn.approve(params.UNISWAP_ROUTER_ADDRESS, ntn_amount).transact()
     w3.eth.wait_for_transaction_receipt(approve_tx_2)
 
     usdc = ERC20(w3, params.USDCX_ADDRESS)
-    usdc_amount = int(0.01 * 10 ** usdc.decimals())
+    usdc_amount = int(10.01 * 10 ** usdc.decimals())
     approve_tx_1 = usdc.approve(params.UNISWAP_ROUTER_ADDRESS, usdc_amount).transact()
     w3.eth.wait_for_transaction_receipt(approve_tx_1)
 

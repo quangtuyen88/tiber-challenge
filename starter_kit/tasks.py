60bfa62594f807bb81f75776bfd4827aaaf34b4c
tquang@mysuki.io <tquang@mysuki.io>
2024-12-21 14:29:01 +0800

    added tet3

--- starter_kit/tasks.py
+++ starter_kit/tasks.py
@@ -57,7 +57,7 @@ def approve(w3: Web3) -> None:
     """Approves the transfer of 0.01 NTN by a recipient specified in .env."""
 
     autonity = Autonity(w3)
-    amount = int(0.01 * 10 ** autonity.decimals())
+    amount = int(0.1 * 10 ** autonity.decimals())
     tx = autonity.approve(params.RECIPIENT_ADDRESS, amount).transact()
     w3.eth.wait_for_transaction_receipt(tx)
 
@@ -68,7 +68,7 @@ def swap_exact_ntn_for_atn(w3: Web3) -> None:
     """Swaps 0.1 NTN for ATN."""
 
     ntn = ERC20(w3, params.NTN_ADDRESS)
-    ntn_amount = int(1 * 10 ** ntn.decimals())
+    ntn_amount = int(0.1 * 10 ** ntn.decimals())
     approve_tx = ntn.approve(params.UNISWAP_ROUTER_ADDRESS, ntn_amount).transact()
     w3.eth.wait_for_transaction_receipt(approve_tx)
 
